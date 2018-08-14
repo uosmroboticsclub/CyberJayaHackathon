fdf0d4e2985f31697d798d14063542a25be18965
UoSM Robotics Club <usmcrobot@gmail.com>
2018-08-14 15:15:24 +0800

    changed led to 3 leds

--- Arduino/Beginning Samples/Beginner/Beginner.ino
+++ Arduino/Beginning Samples/Beginner/Beginner.ino
@@ -1,17 +1,13 @@
-int time_taken = 25;
+int time_taken = 25; //adjust the delay for each led
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(2,OUTPUT);
 pinMode(3,OUTPUT);
 pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(9,OUTPUT);
+
 // or another way
-for (int i = 2; i < 10 ; i++){
+for (int i = 2; i < 5 ; i++){
   pinMode(i,OUTPUT);
 }
 
@@ -19,13 +15,13 @@ for (int i = 2; i < 10 ; i++){
 }
 
 void loop() {
-for (int y =2; y<9 ; y++){                  // begins turning on leds for 1 sec from pin 2 to pin 9
+for (int y =2; y<5 ; y++){                  // begins turning on leds for 1 sec from pin 2 to pin 4
   digitalWrite(y,HIGH);
   delay(time_taken);
   digitalWrite(y,LOW); 
 }
 
-for (int x = 9; x>2 ; x--){                              // begins turning on leds for 1 sec from pin 9 to pin 2
+for (int x = 4; x>2 ; x--){                              // begins turning on leds for 1 sec from pin 4 to pin 2
   digitalWrite(x,HIGH);
   delay(time_taken);
   digitalWrite(x,LOW);
