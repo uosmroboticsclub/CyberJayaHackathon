71d4e5a8314e9ea4d25c63a2dc851e584105cd5f
UoSM Robotics Club <usmcrobot@gmail.com>
2018-08-14 15:19:21 +0800

    Update Flashing.ino

--- Arduino/Beginning Samples/Flashing/Flashing.ino
+++ Arduino/Beginning Samples/Flashing/Flashing.ino
@@ -5,28 +5,23 @@ void setup() {
 pinMode(2,OUTPUT);
 pinMode(3,OUTPUT);
 pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(9,OUTPUT);
+
 // or another way
-for (int i = 2; i < 10 ; i++){
+for (int i = 2; i < 5 ; i++){
   pinMode(i,OUTPUT);
 }
-
 
 }
 
 void loop() {
-for (int y =2; y<10 ; y++){                               // begins turning on leds from pin 2 to pin 9
+for (int y =2; y<5 ; y++){                               // begins turning on leds from pin 2 to pin 9
   digitalWrite(y,HIGH);
   delay(time_taken);
   digitalWrite(y,LOW);
   delay(time_taken);
 }
 
-for (int x = 9; x>1 ; x--){                              // begins turning off leds from pin 9 to pin 2
+for (int x = 4; x>1 ; x--){                              // begins turning off leds from pin 9 to pin 2
   digitalWrite(x,HIGH);
   delay(time_taken);
   digitalWrite(x,LOW);
