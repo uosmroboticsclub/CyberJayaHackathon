0887e89613f224536ecfa6a9d34d94e97c8c7d5f
UoSM Robotics Club <usmcrobot@gmail.com>
2018-08-14 15:16:58 +0800

    Changed led to 3

--- Arduino/Beginning Samples/Experiment/Experiment.ino
+++ Arduino/Beginning Samples/Experiment/Experiment.ino
@@ -5,36 +5,31 @@ void setup() {
 pinMode(2,OUTPUT);
 pinMode(3,OUTPUT);
 pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(9,OUTPUT);
+
 // or another way
-for (int i = 2; i < 10 ; i++){
+for (int i = 2; i < 5 ; i++){
   pinMode(i,OUTPUT);
 }
-
 
 }
 
 void loop() {
-for (int y =2; y<10 ; y++){                               // begins turning on leds from pin 2 to pin 9
+for (int y =2; y<5 ; y++){                               // begins turning on leds from pin 2 to pin 9
   digitalWrite(y,HIGH);
   delay(200);
 }
 
-for (int x = 9; x>1 ; x--){                              // begins turning off leds from pin 9 to pin 2
+for (int x = 4; x>1 ; x--){                              // begins turning off leds from pin 9 to pin 2
   digitalWrite(x,LOW);
   delay(200);
 }
 
-for (int x = 9; x>1 ; x--){                              // begins turning on leds from pin 9 to pin 2
+for (int x = 4; x>1 ; x--){                              // begins turning on leds from pin 9 to pin 2
   digitalWrite(x,HIGH);
   delay(200);
 }
 
-for (int y =2; y<10 ; y++){                               // begins turning OFF leds for 1 sec from pin 2 to pin 9
+for (int y =2; y<5 ; y++){                               // begins turning OFF leds for 1 sec from pin 2 to pin 9
   digitalWrite(y,LOW);
   delay(200);
 }
